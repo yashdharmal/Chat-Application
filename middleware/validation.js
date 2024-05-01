62f72c01855ad4aeb7b4bdf2f5c9b3392e395070
Yash Dharmal <yashdharmal8353@gmail.com>
2024-05-01 19:21:31 +0530

    space added

--- middleware/validation.js
+++ middleware/validation.js
@@ -5,14 +5,12 @@ const validator = (schema) => {
         const { error } = schema.validate(req.body);
 
         if (error) {
-
             const errorMessages = error.details.map((details) => ({ message: details.message }))
             return res.status(403).send(errorMessages);
         }
         next();
     };
 };
-
 
 
 
